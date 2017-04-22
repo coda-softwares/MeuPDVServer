14a6268761ad83fdc8cfb4e9852968c4c6604268
JeffBusterCase <jefferson_bomfim@outlook.com>
2017-04-21 21:20:53 -0300

    commit test

--- MeuPDVServer.py
+++ MeuPDVServer.py
@@ -1,14 +1,21 @@
 from japronto import Application
 import dbmanager as dbm
+import json as JSON
+
+loged = []
 
 def login(req):
     # Estrutura do login
     # Utilizando HMAC
-    params = req.query
+    text = req.body
+    res = req.Response(text=text, mime_type='text/plain')
+    return res
+
 
 def logout(req):
-    pass
+    return req.Response(text)
 
 app = Application()
-
-app.run(debug=True, port=2017)
+app.router.add_route('/login', login)
+app.router.add_route('/logout', logout)
+app.run(debug=True, port=8080)
